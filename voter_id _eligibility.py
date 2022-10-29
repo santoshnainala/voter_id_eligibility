1ea66e4fe8883a68731f16aa726b7d88d1bf8967
santoshnainala <51361959+santoshnainala@users.noreply.github.com>
2022-10-29 21:53:15 +0530

    Add files via upload

---
+++ voter_id _eligibility.py
@@ -0,0 +1,7 @@
+#Eligibility to get a voter id
+age = int(input("Enter the age : "))
+if age<18:
+    print("You are not eligible for the voter id")
+    print("The minimum age required to get voter id is 18 or 18+")
+else:
+    print("You are eligible for the voter id")
